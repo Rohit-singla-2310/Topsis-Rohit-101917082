4dadd1225dd0da61049d7647235587e5b6d95c5d
Rohit Singla <74494475+Rohit-singla-2310@users.noreply.github.com>
2022-02-25 14:23:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="Topsis-Rohit-101917082",
-    version="1.0.0",
+    version="1.0.1",
     description="It calculates topsis score of the data",
     long_description=README,
     long_description_content_type="text/markdown",
